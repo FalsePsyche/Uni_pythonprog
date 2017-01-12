d498b2fe3999f916d62d7d5f8df8b75aabf22f2b
falsepsyche <tanner@tanner.ninja>
2017-01-12 18:51:37 -0500

    attempting unit testing by updating test file

--- cs2021/tests/lab01_tests.py
+++ cs2021/tests/lab01_tests.py
@@ -1,1 +1,12 @@
 # todo add unit tests here so vscode can use its built in python test system
+
+from ../lab1/lab01.py import lab01.py
+
+def _test():
+    import doctest
+    doctest.testmod()
+
+
+if __name__ == "__main__":
+    _test()
+    lab01.factors(20)
